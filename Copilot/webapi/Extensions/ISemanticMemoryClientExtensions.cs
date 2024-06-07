e40112374c6021a494c3469c6f977c0d623d0391
Esben Carlsen <esben@eithify.com>
2024-06-07 16:28:17 +0200

    Refactored semantic memory client extension for dynamic URL configuration
    Refactored the `AddSemanticMemoryServices` method in `ISemanticMemoryClientExtensions.cs` to use dynamic URL configuration from `AiEmpowerLabsOptions`. Also added more pipeline steps in `s_pipelineSteps` and made minor formatting changes for better readability. This enhancement makes the memory service URIs configurable per environment, providing more flexibility.

--- Copilot/webapi/Extensions/ISemanticMemoryClientExtensions.cs
+++ Copilot/webapi/Extensions/ISemanticMemoryClientExtensions.cs
@@ -6,9 +6,13 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
 using CopilotChat.WebApi.Models.Storage;
+using CopilotChat.WebApi.Options;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Microsoft.KernelMemory;
 
 namespace CopilotChat.WebApi.Extensions;
@@ -18,146 +22,154 @@ namespace CopilotChat.WebApi.Extensions;
 /// </summary>
 internal static class SemanticMemoryClientExtensions
 {
-    private static readonly List<string> s_pipelineSteps = ["extract", "partition", "gen_embeddings", "save_records"];
+	private static readonly List<string> s_pipelineSteps = [
+		"extract",
+		"extract_audio",
+		"sanitize",
+		"partition",
+		"gen_embeddings",
+		"save_records"];
 
-    /// <summary>
-    ///     Inject <see cref="IKernelMemory" />.
-    /// </summary>
-    public static void AddSemanticMemoryServices(this WebApplicationBuilder appBuilder)
-    {
-        IKernelMemory memory = new MemoryWebClient(
-            "https://studio.aiempowerlabs.com/api/kernelmemory",
-            "not-needed");
-        appBuilder.Services.AddSingleton(memory);
-    }
+	/// <summary>
+	///     Inject <see cref="IKernelMemory" />.
+	/// </summary>
+	public static void AddSemanticMemoryServices(this WebApplicationBuilder appBuilder)
+	{
+		appBuilder.Services.AddSingleton(sp =>
+		{
+			IOptions<AiEmpowerLabsOptions> options = sp.GetRequiredService<IOptions<AiEmpowerLabsOptions>>();
+			UriBuilder uriBuilder = new(options.Value.Url);
+			uriBuilder.Path += "api/kernelmemory";
+			IKernelMemory memory = new MemoryWebClient(uriBuilder.Uri.ToString(), "not-needed");
+			return memory;
+		});
+	}
 
-    public static Task<SearchResult> SearchMemory(
-        this IKernelMemory memoryClient,
-        string indexName,
-        string query,
-        float relevanceThreshold,
-        string? chatId,
-        string? memoryName = null,
-        CancellationToken cancellationToken = default)
-    {
-        return memoryClient
-            .SearchMemory(indexName, query, relevanceThreshold, -1, chatId, memoryName, cancellationToken);
-    }
+	public static Task<SearchResult> SearchMemory(
+		this IKernelMemory memoryClient,
+		string indexName,
+		string query,
+		float relevanceThreshold,
+		string? chatId,
+		string? memoryName = null,
+		CancellationToken cancellationToken = default)
+	{
+		return memoryClient
+			.SearchMemory(indexName, query, relevanceThreshold, -1, chatId, memoryName, cancellationToken);
+	}
 
-    public static async Task<SearchResult> SearchMemory(
-        this IKernelMemory memoryClient,
-        string indexName,
-        string query,
-        float relevanceThreshold,
-        int resultCount,
-        string? chatId,
-        string? memoryName = null,
-        CancellationToken cancellationToken = default)
-    {
-        MemoryFilter filter = new();
+	public static async Task<SearchResult> SearchMemory(
+		this IKernelMemory memoryClient,
+		string indexName,
+		string query,
+		float relevanceThreshold,
+		int resultCount,
+		string? chatId,
+		string? memoryName = null,
+		CancellationToken cancellationToken = default)
+	{
+		MemoryFilter filter = new();
 
-        if (!string.IsNullOrEmpty(chatId))
-        {
-            filter.ByTag(MemoryTags.TagChatId, chatId);
-        }
+		if (!string.IsNullOrEmpty(chatId))
+		{
+			filter.ByTag(MemoryTags.TagChatId, chatId);
+		}
 
-        if (!string.IsNullOrWhiteSpace(memoryName))
-        {
-            filter.ByTag(MemoryTags.TagMemory, memoryName);
-        }
+		if (!string.IsNullOrWhiteSpace(memoryName))
+		{
+			filter.ByTag(MemoryTags.TagMemory, memoryName);
+		}
 
-        SearchResult searchResult =
-            await memoryClient.SearchAsync(
-                query,
-                indexName,
-                filter,
-                null,
-                relevanceThreshold, // minRelevance param
-                resultCount,
-                cancellationToken);
+		SearchResult searchResult =
+			await memoryClient.SearchAsync(
+				query,
+				indexName,
+				filter,
+				null,
+				relevanceThreshold, // minRelevance param
+				resultCount,
+				cancellationToken);
 
-        return searchResult;
-    }
+		return searchResult;
+	}
 
-    public static async Task StoreDocumentAsync(
-        this IKernelMemory memoryClient,
-        string indexName,
-        string documentId,
-        string chatId,
-        string memoryName,
-        string fileName,
-        Stream fileContent,
-        CancellationToken cancellationToken = default)
-    {
-        DocumentUploadRequest uploadRequest =
-            new()
-            {
-                DocumentId = documentId,
-                Files = [new DocumentUploadRequest.UploadedFile(fileName, fileContent)],
-                Index = indexName,
-                Steps = s_pipelineSteps
-            };
+	public static async Task StoreDocumentAsync(
+		this IKernelMemory memoryClient,
+		string indexName,
+		string documentId,
+		string chatId,
+		string memoryName,
+		string fileName,
+		Stream fileContent,
+		CancellationToken cancellationToken = default)
+	{
+		DocumentUploadRequest uploadRequest =
+			new()
+			{
+				DocumentId = documentId,
+				Files = [new DocumentUploadRequest.UploadedFile(fileName, fileContent)],
+				Index = indexName,
+				Steps = s_pipelineSteps
+			};
 
-        uploadRequest.Tags.Add(MemoryTags.TagChatId, chatId);
-        uploadRequest.Tags.Add(MemoryTags.TagMemory, memoryName);
-
-        await memoryClient.ImportDocumentAsync(uploadRequest, cancellationToken);
-    }
-
-    public static Task StoreMemory(
-        this IKernelMemory memoryClient,
-        string indexName,
-        string chatId,
-        string memoryName,
-        string memory,
-        CancellationToken cancellationToken = default)
-    {
-        return memoryClient.StoreMemory(indexName, chatId, memoryName, Guid.NewGuid().ToString(), memory,
-            cancellationToken);
-    }
-
-    public static async Task StoreMemory(
-        this IKernelMemory memoryClient,
-        string indexName,
-        string chatId,
-        string memoryName,
-        string memoryId,
-        string memory,
-        CancellationToken cancellationToken = default)
-    {
-        using MemoryStream stream = new();
-        await using StreamWriter writer = new(stream);
-        await writer.WriteAsync(memory);
-        await writer.FlushAsync(cancellationToken);
-        stream.Position = 0;
-
-        DocumentUploadRequest uploadRequest = new()
-        {
-            DocumentId = memoryId,
-            Index = indexName,
-            Files =
-                [new DocumentUploadRequest.UploadedFile("memory.txt", stream)],
-            Steps = s_pipelineSteps
-        };
-
-        uploadRequest.Tags.Add(MemoryTags.TagChatId, chatId);
-        uploadRequest.Tags.Add(MemoryTags.TagMemory, memoryName);
+		uploadRequest.Tags.Add(MemoryTags.TagChatId, chatId);
+		uploadRequest.Tags.Add(MemoryTags.TagMemory, memoryName);
 
 		await memoryClient.ImportDocumentAsync(uploadRequest, cancellationToken);
 	}
 
-    public static async Task RemoveChatMemories(
-        this IKernelMemory memoryClient,
-        string indexName,
-        string chatId,
-        CancellationToken cancellationToken = default)
-    {
-        SearchResult memories = await memoryClient
-			.SearchMemory(indexName, "*", 0.0F, chatId, cancellationToken: cancellationToken);
-        string[] documentIds = memories.Results.Select(memory => memory.Link.Split('/').First()).Distinct().ToArray();
-        Task[] tasks = documentIds
-            .Select(documentId => memoryClient.DeleteDocumentAsync(documentId, indexName, cancellationToken)).ToArray();
+	public static Task StoreMemory(
+		this IKernelMemory memoryClient,
+		string indexName,
+		string chatId,
+		string memoryName,
+		string memory,
+		CancellationToken cancellationToken = default)
+	{
+		return memoryClient.StoreMemory(indexName, chatId, memoryName, Guid.NewGuid().ToString(), memory, cancellationToken);
+	}
 
-        Task.WaitAll(tasks, cancellationToken);
-    }
+	private static async Task StoreMemory(
+		this IKernelMemory memoryClient,
+		string indexName,
+		string chatId,
+		string memoryName,
+		string memoryId,
+		string memory,
+		CancellationToken cancellationToken = default)
+	{
+		using MemoryStream stream = new();
+		await using StreamWriter writer = new(stream);
+		await writer.WriteAsync(memory);
+		await writer.FlushAsync(cancellationToken);
+		stream.Position = 0;
+
+		DocumentUploadRequest uploadRequest = new()
+		{
+			DocumentId = memoryId,
+			Index = indexName,
+			Files = [new DocumentUploadRequest.UploadedFile("memory.txt", stream)],
+			Steps = s_pipelineSteps
+		};
+
+		uploadRequest.Tags.Add(MemoryTags.TagChatId, chatId);
+		uploadRequest.Tags.Add(MemoryTags.TagMemory, memoryName);
+
+		await memoryClient.ImportDocumentAsync(uploadRequest, cancellationToken);
+	}
+
+	public static async Task RemoveChatMemories(
+		this IKernelMemory memoryClient,
+		string indexName,
+		string chatId,
+		CancellationToken cancellationToken = default)
+	{
+		SearchResult memories = await memoryClient
+			.SearchMemory(indexName, "*", 0.0F, chatId, cancellationToken: cancellationToken);
+		string[] documentIds = memories.Results.Select(memory => memory.Link.Split('/').First()).Distinct().ToArray();
+		Task[] tasks = documentIds
+			.Select(documentId => memoryClient.DeleteDocumentAsync(documentId, indexName, cancellationToken)).ToArray();
+
+		Task.WaitAll(tasks, cancellationToken);
+	}
 }
