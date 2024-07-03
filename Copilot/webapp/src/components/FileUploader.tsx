516466d65c50b9cab9bfceb3759e3be18e092660
Esben Carlsen <esben@aiempowerlabs.com>
2024-07-03 19:59:59 +0200

    Update dependencies, add chat component, refactor FileUploader
    This commit involves several updates. The most significant involves the addition of a new Chat component. Additionally, numerous dependencies in the package.json file have been updated with newer versions to keep the application up-to-date with the most recent improvements and bug fixes. Refactoring in the FileUploader component has also been completed, mainly revolving around how the component is exported.

--- Copilot/webapp/src/components/FileUploader.tsx
+++ Copilot/webapp/src/components/FileUploader.tsx
@@ -10,45 +10,46 @@ const useClasses = makeStyles({
 interface FileUploaderProps {
     acceptedExtensions: string[] | undefined;
     onSelectedFile: (file: File) => void;
-    ref?: React.Ref<HTMLInputElement> | string;
 }
 
-export const FileUploader: React.FC<FileUploaderProps> = forwardRef<HTMLInputElement, FileUploaderProps>(
-    ({ acceptedExtensions, onSelectedFile }, ref) => {
-        const classes = useClasses();
+const FileUploaderComponent = (
+    { acceptedExtensions, onSelectedFile }: FileUploaderProps,
+    ref: React.ForwardedRef<HTMLInputElement>) => {
+    const classes = useClasses();
 
-        const onChange = React.useCallback(
-            (event: React.SyntheticEvent) => {
-                const target = event.target as HTMLInputElement;
-                const selectedFiles = target.files;
-                event.stopPropagation();
-                event.preventDefault();
-                if (!selectedFiles || selectedFiles.length !== 1) {
-                    console.error('There are none or multiple selected files.');
-                    return;
-                }
-                const file = selectedFiles.item(0);
-                if (file) {
-                    onSelectedFile(file);
-                } else {
-                    console.error('The selected file contains no file object.');
-                }
-            },
-            [onSelectedFile],
-        );
+    const onChange = React.useCallback(
+        (event: React.SyntheticEvent) => {
+            const target = event.target as HTMLInputElement;
+            const selectedFiles = target.files;
+            event.stopPropagation();
+            event.preventDefault();
+            if (!selectedFiles || selectedFiles.length !== 1) {
+                console.error('There are none or multiple selected files.');
+                return;
+            }
+            const file = selectedFiles.item(0);
+            if (file) {
+                onSelectedFile(file);
+            } else {
+                console.error('The selected file contains no file object.');
+            }
+        },
+        [onSelectedFile],
+    );
 
-        return (
-            <input
-                ref={ref}
-                type="file"
-                id="fileInput"
-                className={classes.root}
-                accept={acceptedExtensions?.join(',')}
-                onChange={onChange}
-                title="Upload a .txt or .pdf or .docx or .md or .jpg or .jpeg or .png or .tif or .tiff or .bmp or .gif or .wav or .opus or .mp3 or .acc or .oga or .webm or .mp4 or .ogv or .mpeg or .webm or .ogx file"
-            />
-        );
-    },
-);
+    return (
+        <input
+            ref={ref}
+            type="file"
+            id="fileInput"
+            className={classes.root}
+            accept={acceptedExtensions?.join(',')}
+            onChange={onChange}
+            title="Upload a .txt or .pdf or .docx or .md or .jpg or .jpeg or .png or .tif or .tiff or .bmp or .gif or .wav or .opus or .mp3 or .acc or .oga or .webm or .mp4 or .ogv or .mpeg or .webm or .ogx file"
+        />
+    );
+};
+
+export const FileUploader = forwardRef<HTMLInputElement, FileUploaderProps>(FileUploaderComponent);
 
 FileUploader.displayName = 'FileUploader';
